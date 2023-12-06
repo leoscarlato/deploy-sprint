cc21eb3b99ea94d2e13afbc4139e68d1af2ce8f5
C0D8 <matheusaguiar362@gmail.com>
2023-12-06 15:01:50 -0300

    api com script de regressão com warnings

--- api/app.py
+++ api/app.py
@@ -3,7 +3,11 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import pickle
-from ..notebooks.script_data_basico import tratamento_classificacao
+# from ..scripts.script_data_basico import tratamento_classificacao
+import sys
+sys.path.append('..')
+from scripts.script_data_regressao import tratamento_regressao
+# from ..scripts.script_data_regressao import tratamento_regressao
 
 def transform_to_category(x,qtd_itens,lista_itens):
    for i in range(qtd_itens):
@@ -24,50 +28,50 @@ colunas_dropadas_regr = ['contract_start_date','contract_end_date','id_continuit
                      'Qde Atendimento Médico','Faltas Atendimento Médico',	'Qde Atendimentos Acolhimento',	'Faltas Acolhimento',	'Qde Psicoterapia',	'Faltas Psicoterapia','Data Última Ligações Outbound',\
                      'Data Última Ligações Inbound','Qde Total de Faturas Pagas após Vencimento','Qde Perfis de Pagamento Inativos', 'Valor Médio da Mensalidade', 'status_prox_mes', 'Qde Total de Faturas','Problemas Abertos', "Tempo até Sair"]
 
-#rota para classificação
-@app.route('/classification/predict', methods=['POST'])
-def predict_class():
+# #rota para classificação
+# @app.route('/classification/predict', methods=['POST'])
+# def predict_class():
 
-    try:
-        data = request.get_json()
-        features = data.get('features')
+#     try:
+#         data = request.get_json()
+#         features = data.get('features')
         
-        if not features:
-            return jsonify({'error': 'Dados de entrada não fornecidos'}), 400
+#         if not features:
+#             return jsonify({'error': 'Dados de entrada não fornecidos'}), 400
 
-        features = pd.DataFrame(features, index=[0])
+#         features = pd.DataFrame(features, index=[0])
 
-        print(features.iloc[0])
+#         print(features.iloc[0])-
 
-        print("#"*20)
+#         print("#"*20)
 
-        clean_data_path = "../data/df_total.csv"
+#         clean_data_path = "../data/df_total.csv"
 
-        df = pd.read_csv(clean_data_path)
-        df = df.drop(colunas_dropadas_regr, axis=1)
-        df2 = df.copy()
+#         df = pd.read_csv(clean_data_path)
+#         df = df.drop(colunas_dropadas_regr, axis=1)
+#         df2 = df.copy()
 
-        features = features[df2.columns]
+#         features = features[df2.columns]
 
-        df2 = pd.concat([df2, features], ignore_index=False)
-        model_data = df2.copy()
+#         df2 = pd.concat([df2, features], ignore_index=False)
+#         model_data = df2.copy()
 
-        model_data = tratamento_classificacao(model_data)
+#         model_data = tratamento_classificacao(model_data)
 
-        model_data = model_data.iloc[-1]
-        print(model_data)
+#         model_data = model_data.iloc[-1]
+#         print(model_data)
 
-        modelo_carregado = joblib.load("../notebooks/regression_model.joblib")
+#         modelo_carregado = joblib.load("../notebooks/regression_model.joblib")
 
-        # model_data = model_data.drop('SalePrice')
-        prediction = modelo_carregado.predict(model_data.to_numpy().reshape(1, -1))
-        print(prediction)
-        return jsonify({'prediction': prediction[0]})
+#         # model_data = model_data.drop('SalePrice')
+#         prediction = modelo_carregado.predict(model_data.to_numpy().reshape(1, -1))
+#         print(prediction)
+#         return jsonify({'prediction': prediction[0]})
 
 
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
 
 #Rota para regressão
@@ -90,78 +94,96 @@ def predict():
         clean_data_path = "../data/df_total.csv"
 
         df = pd.read_csv(clean_data_path)
-        df = df.drop(colunas_dropadas_regr, axis=1)
+        # df = df.drop(colunas_dropadas_regr, axis=1)
         df2 = df.copy()
 
-        features = features[df2.columns]
+        # features = features[df2.columns]
 
         df2 = pd.concat([df2, features], ignore_index=False)
+
+        df2['Target'] = 0
+        df2['id_person'] = 0
+        
         model_data = df2.copy()
+
+        model_data = tratamento_regressao(model_data)[0]
 
         ##############################################
                 
-        colunas_get_dummies = ['id_gender','id_marrital_status','id_health_plan','notes_count','Método de Pagamento','Qde Total de Faturas Inadimpletes', 'Quem Enviou Última Mensagem']
+        # colunas_get_dummies = ['id_gender','id_marrital_status','id_health_plan','notes_count','Método de Pagamento','Qde Total de Faturas Inadimpletes', 'Quem Enviou Última Mensagem']
 
-        #coluna de genero
-        model_data['id_gender'] = model_data['id_gender'] = model_data['id_gender'].apply(lambda x: transform_to_category(x,2,[63,64]))
-        #coluna de estado civil
-        #drop marrital status diferentes de 80,82,83
-        model_data = model_data[model_data['id_marrital_status'].isin([80,82,83])]
-        print("a#"*20)
-        print(model_data.iloc[-1])
-        print("#"*20)
+        # #coluna de genero
+        # model_data['id_gender'] = model_data['id_gender'] = model_data['id_gender'].apply(lambda x: transform_to_category(x,2,[63,64]))
+        # #coluna de estado civil
+        # #drop marrital status diferentes de 80,82,83
+        # model_data = model_data[model_data['id_marrital_status'].isin([80,82,83])]
+        # print("a#"*20)
+        # print(model_data.iloc[-1])
+        # print("#"*20)
 
 
-        model_data['id_marrital_status'] = model_data['id_marrital_status'].apply(lambda x: transform_to_category(x,3,[80,82,83]))
-        #coluna de health plan
-        model_data['id_health_plan'] = model_data['id_health_plan'].apply(lambda x: transform_to_category(x,4,[412,415,418,435]))
-        #coluna de notes count
-        model_data = model_data[model_data['notes_count'] < 7]
+        # model_data['id_marrital_status'] = model_data['id_marrital_status'].apply(lambda x: transform_to_category(x,3,[80,82,83]))
+        # #coluna de health plan
+        # model_data['id_health_plan'] = model_data['id_health_plan'].apply(lambda x: transform_to_category(x,4,[412,415,418,435]))
+        # #coluna de notes count
+        # model_data = model_data[model_data['notes_count'] < 7]
 
-        #coluna birthdate
-        model_data = model_data[model_data['birthdate'].notna()]
-        #coluna de fisico
-        model_data['Físico'] = model_data['Físico'].fillna(model_data['Físico'].mean())
-        #coluna de psicologico
-        model_data['Psicológico'] = model_data['Psicológico'].fillna(model_data['Psicológico'].mean())
-        #coluna de social 
-        model_data['Social'] = model_data['Social'].fillna(model_data['Social'].mean())
-        #coluna de ambiental
-        model_data['Ambiental'] = model_data['Ambiental'].fillna(model_data['Ambiental'].mean())
+        # #coluna birthdate
+        # model_data = model_data[model_data['birthdate'].notna()]
+        # #coluna de fisico
+        # model_data['Físico'] = model_data['Físico'].fillna(model_data['Físico'].mean())
+        # #coluna de psicologico
+        # model_data['Psicológico'] = model_data['Psicológico'].fillna(model_data['Psicológico'].mean())
+        # #coluna de social 
+        # model_data['Social'] = model_data['Social'].fillna(model_data['Social'].mean())
+        # #coluna de ambiental
+        # model_data['Ambiental'] = model_data['Ambiental'].fillna(model_data['Ambiental'].mean())
 
-        #coluna de mensagens inbound
-        model_data['Mensagens Inbound'] = model_data['Mensagens Inbound'].fillna(0)
-        #coluna de mensagens outbound
-        model_data['Mensagens Outbound'] = model_data['Mensagens Outbound'].fillna(0)
-        #coluna de ligacoes inbound
-        model_data['Ligações Inbound'] = model_data['Ligações Inbound'].fillna(0)
-        #coluna de ligacoes outbound
-        model_data['Ligações Outbound'] = model_data['Ligações Outbound'].fillna(0)
-        #coluna qtd tentativas de cobrança
-        model_data['Qde Total de Tentativas de Cobrança'] = model_data['Qde Total de Tentativas de Cobrança'].fillna(0)
+        # #coluna de mensagens inbound
+        # model_data['Mensagens Inbound'] = model_data['Mensagens Inbound'].fillna(0)
+        # #coluna de mensagens outbound
+        # model_data['Mensagens Outbound'] = model_data['Mensagens Outbound'].fillna(0)
+        # #coluna de ligacoes inbound
+        # model_data['Ligações Inbound'] = model_data['Ligações Inbound'].fillna(0)
+        # #coluna de ligacoes outbound
+        # model_data['Ligações Outbound'] = model_data['Ligações Outbound'].fillna(0)
+        # #coluna qtd tentativas de cobrança
+        # model_data['Qde Total de Tentativas de Cobrança'] = model_data['Qde Total de Tentativas de Cobrança'].fillna(0)
 
-        #coluna método de pagamento
-        model_data['Método de Pagamento'] = model_data['Método de Pagamento'].apply(lambda x: transform_to_category(x,2,["Cartão de crédito","Dinheiro"]))
-        #coluna total de faturas inadimplentes
-        model_data['Qde Total de Faturas Inadimpletes'] = model_data['Qde Total de Faturas Inadimpletes'].fillna(0)
-        model_data['Qde Total de Faturas Inadimpletes'] = model_data['Qde Total de Faturas Inadimpletes'].apply(lambda x: True if x > 0 else False)
+        # #coluna método de pagamento
+        # model_data['Método de Pagamento'] = model_data['Método de Pagamento'].apply(lambda x: transform_to_category(x,2,["Cartão de crédito","Dinheiro"]))
+        # #coluna total de faturas inadimplentes
+        # model_data['Qde Total de Faturas Inadimpletes'] = model_data['Qde Total de Faturas Inadimpletes'].fillna(0)
+        # model_data['Qde Total de Faturas Inadimpletes'] = model_data['Qde Total de Faturas Inadimpletes'].apply(lambda x: True if x > 0 else False)
 
-        #coluna valor total inadimplente
-        model_data['Valor Total Inadimplência'] = model_data['Valor Total Inadimplência'].fillna(0)
-        #coluna Tempo Última Mensagem Inbound
-        model_data['Tempo Última Mensagem Inbound'] = model_data['Tempo Última Mensagem Inbound'].fillna(0)
-        #coluna Tempo Última Mensagem Outbound
-        model_data['Tempo Última Mensagem Outbound'] = model_data['Tempo Última Mensagem Outbound'].fillna(0)
-        #rename coluna birthdate para idade
-        model_data = model_data.rename(columns={'birthdate':'idade'})
+        # #coluna valor total inadimplente
+        # model_data['Valor Total Inadimplência'] = model_data['Valor Total Inadimplência'].fillna(0)
+        # #coluna Tempo Última Mensagem Inbound
+        # model_data['Tempo Última Mensagem Inbound'] = model_data['Tempo Última Mensagem Inbound'].fillna(0)
+        # #coluna Tempo Última Mensagem Outbound
+        # model_data['Tempo Última Mensagem Outbound'] = model_data['Tempo Última Mensagem Outbound'].fillna(0)
+        # #rename coluna birthdate para idade
+        # model_data = model_data.rename(columns={'birthdate':'idade'})
 
-        # dropa linhas duplicadas
+        # # dropa linhas duplicadas
 
-        model_data = pd.get_dummies(model_data, columns=colunas_get_dummies)
+        # model_data = pd.get_dummies(model_data, columns=colunas_get_dummies)
 
         modelo_carregado = joblib.load("../notebooks/regression_model.joblib")
 
+        print('a' * 20)
+        print(model_data)
+
+        #dropar coluna Tempo até Sair de model_data
+
+        model_data = model_data.drop('Tempo até Sair', axis=1)
+
+
+        print('b' * 20)
+
+
         model_data = model_data.iloc[-1]
+        
         print(model_data)
 
         # model_data = model_data.drop('SalePrice')
